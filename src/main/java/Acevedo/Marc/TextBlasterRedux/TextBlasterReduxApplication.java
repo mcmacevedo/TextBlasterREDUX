0b019a68443ce428ade2a43493048581dae137f8
Marc Acevedo <mcmacevedo@gmail.com>
2022-01-28 13:31:27 -0500

    Added logging with application name

--- src/main/java/Acevedo/Marc/TextBlasterRedux/TextBlasterReduxApplication.java
+++ src/main/java/Acevedo/Marc/TextBlasterRedux/TextBlasterReduxApplication.java
@@ -1,12 +1,19 @@
 package Acevedo.Marc.TextBlasterRedux;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TextBlasterReduxApplication {
+
+	@Value(value = "${spring.application.name}")
+	static String applicationName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TextBlasterReduxApplication.class, args);
+		log.info("Starting up {}", applicationName);
 	}
 }
